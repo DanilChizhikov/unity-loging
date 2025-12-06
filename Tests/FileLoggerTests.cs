fbe484f7f9c4d3384b4bd5117d90079e421d65bf
DanilChizhikov <clichindustries@gmail.com>
2025-12-06 12:01:09 +0700

    updated tests

---
+++ Tests/FileLoggerTests.cs
@@ -0,0 +1,107 @@
+using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace DTech.Logging.Tests
+{
+	[TestFixture]
+	internal sealed class FileLoggerTests
+	{
+		private LogSettingsWrapper _settingsWrapper;
+		
+		[OneTimeSetUp]
+		public void SetUp()
+		{
+			_settingsWrapper = new LogSettingsWrapper(LoggerSettings.Instance);
+		}
+
+		[OneTimeTearDown]
+		public void TearDown()
+		{
+			_settingsWrapper.ResetSettings();
+		}
+		
+		[Test]
+		public void Log_WithEnabledFileLogging_WritesToFile()
+		{
+			_settingsWrapper.OverrideInformationEnabled(true)
+				.OverrideFileLoggingEnabled(true);
+			
+			var logger = new CapturingFileLogger("TestTag");
+			
+			logger.LogInfo("Hello World");
+			
+			Assert.IsTrue(File.Exists(LoggerFileProvider.CurrentLogFilePath));
+		}
+		
+		[Test]
+		public void Log_WithDisabledFileLogging_DoesNotWriteToLog()
+		{
+			_settingsWrapper.OverrideFileLoggingEnabled(false);
+			
+			var logger = new CapturingFileLogger("TestTag");
+			logger.LogInfo("Hello World");
+			
+			Assert.IsFalse(File.Exists(LoggerFileProvider.CurrentLogFilePath));
+		}
+		
+		private sealed class CapturingFileLogger : InternalLoggerBase
+		{
+			public CapturingFileLogger(string tag) : base(tag)
+			{
+			}
+
+			public override bool IsEnabled(LogLevel logLevel)
+			{
+				return LogConditions.IsFileLoggingEnabled &&
+					LoggerSettings.Instance.IsFileLoggingEnabled &&
+					LoggerSettings.Instance.IsEnabled(logLevel);
+			}
+
+			protected override void SendLog<TState>(LogLevel logLevel, Exception exception, Func<Exception, string> formatter, string scopes)
+			{
+				if (!LoggerSettings.Instance.IsFileLoggingEnabled)
+				{
+					return;
+				}
+			
+				using var stream = new StreamWriter(LoggerFileProvider.CurrentLogFilePath, true);
+				string level = logLevel.ToString().ToUpperInvariant();
+				switch (logLevel)
+				{
+					case LogLevel.Information:
+					case LogLevel.Warning:
+					case LogLevel.Critical:
+					{
+						level = level.Substring(0, 4);
+					} break;
+				}
+
+				string stateName = typeof(TState).Name;
+				bool isNullState = stateName == NullStateName;
+				if (string.IsNullOrEmpty(Tag))
+				{
+					if (isNullState)
+					{
+						stream.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}][{level}]{scopes} {formatter(exception)}");
+					}
+					else
+					{
+						stream.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}][{level}]{scopes}[{stateName}] {formatter(exception)}");	
+					}
+				}
+				else
+				{
+					if (isNullState)
+					{
+						stream.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}][{level}]{scopes}[{Tag}] {formatter(exception)}");
+					}
+					else
+					{
+						stream.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}][{level}]{scopes}[{Tag}][{stateName}] {formatter(exception)}");
+					}
+				}
+			}
+		}
+	}
+}
--- Tests/LoggerSettingsTests.cs
+++ Tests/LoggerSettingsTests.cs
@@ -1,4 +1,3 @@
-using System.Reflection;
 using NUnit.Framework;
 
 namespace DTech.Logging.Tests
@@ -6,50 +5,39 @@ namespace DTech.Logging.Tests
     [TestFixture]
     internal sealed class LoggerSettingsTests
     {
-        private LogSettingsInfo _backupSettings;
+        private LogSettingsWrapper _settingsWrapper;
 
         [OneTimeSetUp]
         public void SetUp()
         {
-            _backupSettings = GetBackupSettings();
+            _settingsWrapper = GetBackupSettings();
         }
 
         [OneTimeTearDown]
         public void TearDown()
         {
-            LoggerSettings settings = LoggerSettings.Instance;
-            SetPrivateBoolField(settings, LogSettingsInfo.IsTraceEnabledFieldName, _backupSettings.IsTraceEnabled);
-            SetPrivateBoolField(settings, LogSettingsInfo.IsDebugEnabledFieldName, _backupSettings.IsDebugEnabled);
-            SetPrivateBoolField(settings, LogSettingsInfo.IsInformationEnabledFieldName, _backupSettings.IsInformationEnabled);
-            SetPrivateBoolField(settings, LogSettingsInfo.IsWarningEnabledFieldName, _backupSettings.IsWarningEnabled);
-            SetPrivateBoolField(settings, LogSettingsInfo.IsErrorEnabledFieldName, _backupSettings.IsErrorEnabled);
-            SetPrivateBoolField(settings, LogSettingsInfo.IsCriticalEnabledFieldName, _backupSettings.IsCriticalEnabled);
-            SetPrivateBoolField(settings, LogSettingsInfo.IsFileLoggingEnabledFieldName, _backupSettings.IsFileLoggingEnabled);
+            _settingsWrapper.ResetSettings();
         }
 
         [Test]
         public void IsFileLoggingEnabled_ReturnsBackingField()
         {
-            var settings = LoggerSettings.Instance;
-
-            SetPrivateBoolField(settings, LogSettingsInfo.IsFileLoggingEnabledFieldName, true);
+            _settingsWrapper.OverrideFileLoggingEnabled(true);
             Assert.IsTrue(LoggerSettings.Instance.IsFileLoggingEnabled);
 
-            SetPrivateBoolField(settings, LogSettingsInfo.IsFileLoggingEnabledFieldName, false);
+            _settingsWrapper.OverrideFileLoggingEnabled(false);
             Assert.IsFalse(LoggerSettings.Instance.IsFileLoggingEnabled);
         }
 
         [Test]
         public void IsEnabled_RespectsEachLogLevelFlag()
         {
-            var settings = LoggerSettings.Instance;
-
-            SetPrivateBoolField(settings, LogSettingsInfo.IsTraceEnabledFieldName, false);
-            SetPrivateBoolField(settings, LogSettingsInfo.IsDebugEnabledFieldName, false);
-            SetPrivateBoolField(settings, LogSettingsInfo.IsInformationEnabledFieldName, false);
-            SetPrivateBoolField(settings, LogSettingsInfo.IsWarningEnabledFieldName, false);
-            SetPrivateBoolField(settings, LogSettingsInfo.IsErrorEnabledFieldName, false);
-            SetPrivateBoolField(settings, LogSettingsInfo.IsCriticalEnabledFieldName, false);
+            _settingsWrapper.OverrideTraceEnabled(false)
+                .OverrideDebugEnabled(false)
+                .OverrideInformationEnabled(false)
+                .OverrideWarningEnabled(false)
+                .OverrideErrorEnabled(false)
+                .OverrideCriticalEnabled(false);
 
             Assert.IsFalse(LoggerSettings.Instance.IsEnabled(LogLevel.Trace));
             Assert.IsFalse(LoggerSettings.Instance.IsEnabled(LogLevel.Debug));
@@ -58,72 +46,29 @@ namespace DTech.Logging.Tests
             Assert.IsFalse(LoggerSettings.Instance.IsEnabled(LogLevel.Error));
             Assert.IsFalse(LoggerSettings.Instance.IsEnabled(LogLevel.Critical));
 
-            SetPrivateBoolField(settings, LogSettingsInfo.IsTraceEnabledFieldName, true);
+            _settingsWrapper.OverrideTraceEnabled(true);
             Assert.IsTrue(LoggerSettings.Instance.IsEnabled(LogLevel.Trace));
 
-            SetPrivateBoolField(settings, LogSettingsInfo.IsDebugEnabledFieldName, true);
+            _settingsWrapper.OverrideDebugEnabled(true);
             Assert.IsTrue(LoggerSettings.Instance.IsEnabled(LogLevel.Debug));
 
-            SetPrivateBoolField(settings, LogSettingsInfo.IsInformationEnabledFieldName, true);
+            _settingsWrapper.OverrideInformationEnabled(true);
             Assert.IsTrue(LoggerSettings.Instance.IsEnabled(LogLevel.Information));
 
-            SetPrivateBoolField(settings, LogSettingsInfo.IsWarningEnabledFieldName, true);
+            _settingsWrapper.OverrideWarningEnabled(true);
             Assert.IsTrue(LoggerSettings.Instance.IsEnabled(LogLevel.Warning));
 
-            SetPrivateBoolField(settings, LogSettingsInfo.IsErrorEnabledFieldName, true);
+            _settingsWrapper.OverrideErrorEnabled(true);
             Assert.IsTrue(LoggerSettings.Instance.IsEnabled(LogLevel.Error));
 
-            SetPrivateBoolField(settings, LogSettingsInfo.IsCriticalEnabledFieldName, true);
+            _settingsWrapper.OverrideCriticalEnabled(true);
             Assert.IsTrue(LoggerSettings.Instance.IsEnabled(LogLevel.Critical));
         }
 
-        private static LogSettingsInfo GetBackupSettings()
+        private static LogSettingsWrapper GetBackupSettings()
         {
             var instance = LoggerSettings.Instance;
-            return new LogSettingsInfo(instance);
-        }
-
-        private static void SetPrivateBoolField(object obj, string fieldName, bool value)
-        {
-            var field = obj.GetType()
-                .GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
-            field?.SetValue(obj, value);
-        }
-        
-        private readonly struct LogSettingsInfo
-        {
-            public const string IsTraceEnabledFieldName = "_isTraceEnabled";
-            public const string IsDebugEnabledFieldName = "_isDebugEnabled";
-            public const string IsInformationEnabledFieldName = "_isInformationEnabled";
-            public const string IsWarningEnabledFieldName = "_isWarningEnabled";
-            public const string IsErrorEnabledFieldName = "_isErrorEnabled";
-            public const string IsCriticalEnabledFieldName = "_isCriticalEnabled";
-            public const string IsFileLoggingEnabledFieldName = "_isFileLoggingEnabled";
-            
-            public bool IsTraceEnabled { get; }
-            public bool IsDebugEnabled { get; }
-            public bool IsInformationEnabled { get; }
-            public bool IsWarningEnabled { get; }
-            public bool IsErrorEnabled { get; }
-            public bool IsCriticalEnabled { get; }
-            public bool IsFileLoggingEnabled { get; }
-
-            public LogSettingsInfo(LoggerSettings settings)
-            {
-                IsTraceEnabled = GetPrivateBool(settings, IsTraceEnabledFieldName);
-                IsDebugEnabled = GetPrivateBool(settings, IsDebugEnabledFieldName);
-                IsInformationEnabled = GetPrivateBool(settings, IsInformationEnabledFieldName);
-                IsWarningEnabled = GetPrivateBool(settings, IsWarningEnabledFieldName);
-                IsErrorEnabled = GetPrivateBool(settings, IsErrorEnabledFieldName);
-                IsCriticalEnabled = GetPrivateBool(settings, IsCriticalEnabledFieldName);
-                IsFileLoggingEnabled = GetPrivateBool(settings, IsFileLoggingEnabledFieldName);
-            }
-
-            private static bool GetPrivateBool(object obj, string fieldName)
-            {
-                FieldInfo field = typeof(LoggerSettings).GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
-                return (bool)field.GetValue(obj);
-            }
+            return new LogSettingsWrapper(instance);
         }
     }
 }
